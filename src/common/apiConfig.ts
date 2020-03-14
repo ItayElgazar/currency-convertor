2805e447ebe3cfe6b182a03393df9a56876d9316
ItayElgazar <itayekk1@gmail.com>
2020-03-14 17:42:27 +0100

    chore: Add apiConfig & wrapper for basic fetch to support types

---
+++ src/common/apiConfig.ts
@@ -0,0 +1,11 @@
+const functions = {
+  CURRENCY_EXCHANGE_RATE: 'CURRENCY_EXCHANGE_RATE'
+};
+
+const API_CONFIG = {
+  key: 'EKUVGHHIT3W9INJM',
+  baseUrl: 'https://www.alphavantage.co/query?function=',
+  functions
+};
+
+export default API_CONFIG;
