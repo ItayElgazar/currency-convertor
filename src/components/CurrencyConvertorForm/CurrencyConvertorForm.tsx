87621dae75b92d8b21ea28193ac712432830d6f8
ItayElgazar <itayekk1@gmail.com>
2020-03-15 21:06:24 +0100

    feat: Add convertor form component & it's styling

--- src/components/CurrencyConvertorForm/CurrencyConvertorForm.tsx
+++ src/components/CurrencyConvertorForm/CurrencyConvertorForm.tsx
@@ -1,14 +1,108 @@
-import React, { FC } from 'react';
+import React, { FC, useState, useEffect, ChangeEvent } from 'react';
+import CurrencySelect from '../CurrencySelect/CurrencySelect';
 import { ConvertEvent } from '../../store/CurrencyConvertor/types';
+import {
+  Card,
+  CardContent,
+  TextField,
+  CardHeader,
+  Container,
+  makeStyles,
+  Theme,
+  createStyles,
+  Button,
+  IconButton
+} from '@material-ui/core';
+import { SwapHoriz } from '@material-ui/icons';
 
 type CurrencyConvertorFormProps = {
   onConvert: (convertInformation: ConvertEvent) => void;
 };
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      '& .MuiTextField-root': {
+        margin: theme.spacing(1),
+        [theme.breakpoints.down('sm')]: {
+          width: '100%',
+          marginLeft: 0
+        }
+      }
+    },
+    swap: {
+      [theme.breakpoints.down('sm')]: {
+        display: 'none'
+      }
+    }
+  })
+);
+
 const CurrencyConvertorForm: FC<CurrencyConvertorFormProps> = ({
   onConvert
 }) => {
-  return <div>FORM_HERE</div>;
+  const classes = useStyles();
+
+  const [fromCurrency, setFromCurrency] = useState('EUR');
+  const [toCurrency, setToCurrency] = useState('ILS');
+
+  const handleFromCurrencyChange = (currency: string) => {
+    setFromCurrency(currency);
+  };
+
+  const handleToCurrencyChange = (currency: string) => {
+    setToCurrency(currency);
+  };
+
+  const handleSwap = () => {
+    setFromCurrency(toCurrency);
+    setToCurrency(fromCurrency);
+  };
+
+  useEffect(() => {
+    onConvert({
+      fromCurrency,
+      toCurrency
+    });
+  }, [fromCurrency, toCurrency, onConvert]);
+
+  console.log(fromCurrency, toCurrency);
+  return (
+    <Container maxWidth="md" className={classes.root}>
+      <Card>
+        <CardHeader title="Currency Convertor" />
+        <CardContent>
+          <TextField
+            label="Amount"
+            defaultValue={1}
+            variant="outlined"
+            type="number"
+          />
+
+          <CurrencySelect
+            label="From"
+            initialCurrency={fromCurrency}
+            onCurrencySelected={handleFromCurrencyChange}
+          />
+          <IconButton
+            className={classes.swap}
+            color="primary"
+            aria-label="Swap"
+            component="span"
+            onClick={handleSwap}
+          >
+            <SwapHoriz />
+          </IconButton>
+
+          <CurrencySelect
+            label="To"
+            initialCurrency={toCurrency}
+            onCurrencySelected={handleToCurrencyChange}
+          />
+        </CardContent>
+      </Card>
+    </Container>
+  );
 };
 
 export default CurrencyConvertorForm;
---
+++ src/components/CurrencySelect/CurrencySelect.tsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect, FC, ChangeEvent } from 'react';
+import {
+  TextField,
+  MenuItem,
+  makeStyles,
+  createStyles,
+  Theme
+} from '@material-ui/core';
+import CURRENCY_LIST from '../../common/currencyList';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    currencySelect: {
+      width: 265
+    }
+  })
+);
+
+type CurrencySelectProps = {
+  onCurrencySelected: (currency: string) => void;
+  initialCurrency?: string;
+  label?: string;
+};
+
+const CurrencySelect: FC<CurrencySelectProps> = ({
+  onCurrencySelected,
+  initialCurrency,
+  label
+}) => {
+  const [selectedCurrency, setSelectedCurrency] = useState(initialCurrency);
+
+  useEffect(() => {
+    setSelectedCurrency(initialCurrency);
+  }, [initialCurrency]);
+  const classes = useStyles();
+
+  const handleCurrencySelect = ({ target }: ChangeEvent<HTMLInputElement>) => {
+    setSelectedCurrency(target.value);
+    onCurrencySelected(target.value);
+  };
+  return (
+    <TextField
+      className={classes.currencySelect}
+      select
+      label="Select"
+      onChange={handleCurrencySelect}
+      helperText={label}
+      variant="outlined"
+      value={selectedCurrency}
+    >
+      {CURRENCY_LIST.map(option => (
+        <MenuItem key={option.code} value={option.code}>
+          {option.name} - {option.code}
+        </MenuItem>
+      ))}
+    </TextField>
+  );
+};
+
+export default CurrencySelect;
